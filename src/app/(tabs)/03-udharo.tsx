85ef319d1dc5dcb0345ccd1d3cfbf9cf6ad4ee01
tofu-10 <rijanstha.lm10@gmail.com>
2026-07-22 14:45:47 +0545

    Updated the code to use components from the shared folder

--- src/app/(tabs)/03-udharo.tsx
+++ src/app/(tabs)/03-udharo.tsx
@@ -1,2 +1,27 @@
-export { default } from "@/features/udharo/UdharoScreen";
+import UdharoScreen from "@/features/udharo/UdharoScreen";
+import { Colors_UdharoPage } from "@/shared/constants/colors";
+import { FontWeight } from "@/shared/constants/typography";
+import { StyleSheet, Text, View } from "react-native";
 
+export default function UdharoRoute() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Udharo</Text>
+      <UdharoScreen/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors_UdharoPage.background,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: FontWeight.semibold,
+    color: Colors_UdharoPage.textSecondary,
+  },
+});
